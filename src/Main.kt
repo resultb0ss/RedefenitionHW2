018b5eac5db41cdda01e2f21fe7493afd5089b51
Admin <reteamboss@gmail.com>
2024-08-29 17:20:19 +0300

    Задание 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("Hello World!")
+    var array = arrayOf("СТРОКА 1","создать класс","массив строк","самое длинное слово","класс наследует свойства родителя")
+    var maxStr = array[0]
+    for (str in array.indices){
+        if(maxStr.length < array[str].length){
+            maxStr = array[str]
+        }
+
+    }
+    println("Самая длинная строка в масиве -- $maxStr --, ее размер ${maxStr.length} символов")
 }
